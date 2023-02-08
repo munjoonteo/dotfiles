8d8d7e37dc42bb6309fbdba4f188e46cbcce34db
Mun Joon Teo <teomunjoon101@gmail.com>
2023-02-08 22:40:55 +1100

    Removed vim config and added nvim config

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -5,17 +5,13 @@ dotfiles_dir=~/dotfiles
 sudo rm -rf ~/.zshrc > /dev/null 2>&1
 sudo rm -rf ~/.gitconfig > /dev/null 2>&1
 sudo rm -rf ~/.p10k.zsh > /dev/null 2>&1
-sudo rm -rf ~/.vimrc > /dev/null 2>&1
-sudo rm -rf ~/.config/coc/extensions/package.json > /dev/null 2>&1
-sudo rm -rf ~/.vim/coc-settings.json > /dev/null 2>&1
+sudo rm -rf ~/.config/nvim > /dev/null 2>&1
 
-mkdir -p ./config/coc/extensions
-mkdir ~/.vim
+mkdir ~/.config
+mkdir ~/.config/nvim
 
 ln -sf $dotfiles_dir/.zshrc ~/.zshrc
 ln -sf $dotfiles_dir/.gitconfig ~/.gitconfig
 ln -sf $dotfiles_dir/.p10k.zsh ~/.p10k.zsh
-ln -sf $dotfiles_dir/.vimrc ~/.vimrc
-ln -sf $dotfiles_dir/package.json ~/.config/coc/extensions/package.json
-ln -sf $dotfiles_dir/coc-settings.json ~/.vim/coc-settings.json
+ln -sf $dotfiles_dir/init.lua ~/.config/nvim/init.lua
 
