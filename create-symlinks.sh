8e7a907e0e589a3db876b766335f7bd481e8197c
Mun Joon Teo <teomunjoon101@gmail.com>
2023-07-24 20:02:17 +1000

    Add tmux config

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -6,12 +6,13 @@ sudo rm -rf ~/.zshrc > /dev/null 2>&1
 sudo rm -rf ~/.gitconfig > /dev/null 2>&1
 sudo rm -rf ~/.p10k.zsh > /dev/null 2>&1
 sudo rm -rf ~/.config/nvim > /dev/null 2>&1
+sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
 
-mkdir ~/.config
-mkdir ~/.config/nvim
+mkdir -p ~/.config/nvim
 
 ln -sf $dotfiles_dir/.zshrc ~/.zshrc
 ln -sf $dotfiles_dir/.gitconfig ~/.gitconfig
 ln -sf $dotfiles_dir/.p10k.zsh ~/.p10k.zsh
 ln -sf $dotfiles_dir/init.lua ~/.config/nvim/init.lua
+ln -sf $dotfiles_dir/.tmux.conf ~/.tmux.conf
 
