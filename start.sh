87c00cf6d9cb9448c52c9720dc94f03fd54d02ec
Mun Joon Teo <teomunjoon101@gmail.com>
2025-11-13 20:56:46 +0900

    Update dotfiles November 2025

---
+++ start.sh
@@ -0,0 +1,21 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install neovim \
+bat \
+fd \
+fzf \
+git-delta \
+kubectx \
+kubernetes-cli \
+luajit \
+ncurses \
+powerlevel10k \
+pv \
+ripgrep \
+thefuck \
+uv \
+zoxide \
+zsh-autosuggestions \
+zsh-syntax-highlighting
+
+brew install ghostty
